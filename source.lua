d273cb5e5d9f486e3532d51fe12d23e185aa5dd0
Jeremy Clarkson <95444429+dwordsystem@users.noreply.github.com>
2022-07-11 12:55:30 +0200

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,54 @@
+local Services = {
+	Functions = {};	
+	Players = game:GetService("Players");
+	Lighting = game:GetService("Lighting");
+	RunService = game:GetService("RunService");
+	ReplicatedStorage = game:GetService("ReplicatedStorage");
+	TweenService = game:GetService("TweenService");
+};
+
+Services.Functions.Get = function(path:Instance)
+	return game:GetService(path);
+end;
+
+Services.Functions.GetPlayers = function()
+	return game:GetService("Players"):GetPlayers();
+end;
+
+Services.Tween = function(item:Instance, options)
+	if type(options) ~= "table" then
+		if typeof(item) ~= "Instance" then assert(typeof(item)=="table", "Wrong instance input!") end;
+		options.TweenInfo = TweenInfo.new(0.5);
+		options.Goal = {Transparency = 1};
+	end;
+	
+	spawn(function()
+		Services.TweenService:Create(item, options.TweenInfo, options.Goal);
+	end);
+end
+
+Services.EditProperty = function(path:Instance, property, value:any)
+	coroutine.wrap(function()
+		path[property] = value;
+	end);
+end;
+
+Services.RunServiceLoop = function(selection:string, callback)
+	if type(selection) ~= "string" then 
+		selection = "BindToRenderStep";
+	end;
+	if type(callback) ~= "function" then
+		assert(type(callback)=="function", "Wrong callback!");
+	end;
+	
+	if selection == "BindToRenderStep" then
+		Services.RunService:BindToRenderStep(math.random(1, 99999), 0, callback);
+	elseif selection == "RenderStepped" then
+		Services.RunService.RenderStepped:Connect(callback);
+	elseif selection == "Heartbeat" then
+		Services.RunService.Heartbeat:Connect(callback);
+	elseif selection == "Stepped" then
+		Services.RunService.Stepped:Connect(callback);
+	end;
+end;
+
